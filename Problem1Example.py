c79e8451bf90337d2174246cab6f50e39f62f918
r-spiewak <63987228+r-spiewak@users.noreply.github.com>
2024-11-26 15:13:22 -0500

    Add timing to Problem 1 Example script

--- Problem1Example.py
+++ Problem1Example.py
@@ -1,6 +1,7 @@
 """This file runs the Problem 1 Example and
 writes the output to a separate text file."""
 
+import time
 from pathlib import Path
 
 from kinetic_project.graphs.generate_random_graph import (
@@ -12,10 +13,13 @@ from kinetic_project.graphs.graph_ops import iterate_subgraphs
 def main():
     """The main function to be run."""
     G = generate_random_directed_graph(v=10, e=20, seed=2)
+    start_time = time.time()
     subgraphs = iterate_subgraphs(G, k=10, v=2)
+    print(f"Subgraph iteration time: {(time.time()-start_time)/60} min")
     outfilename = Path(__file__).name
     with open(outfilename, "r", encoding="utf8") as file:
         file.write(subgraphs)
+    print(f"Total script time: {(time.time()-start_time)/60} min")
 
 
 if __name__ == "__main__":
